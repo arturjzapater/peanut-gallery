a74536e83216ec60a23437b34a4911259cd595e7
Artur J Zapater <savagepixie@protonmail.com>
2020-07-16 11:45:04 +0200

    all basic functionality done

--- src/routes/films/index.js
+++ src/routes/films/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
-const { handleGetFilm, handleSearch } = require('./handlers')
+const { handleGetFilm, handlePostReview, handleSearch } = require('./handlers')
 
 module.exports = client => {
     router.route('/films/:id')
         .get(handleGetFilm(client))
+
+    router.route('/films/:id/reviews')
+        .post(handlePostReview(client))
 
     router.route('/search')
         .get(handleSearch)
